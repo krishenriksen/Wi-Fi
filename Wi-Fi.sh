6ab50bf9e86dc7ade63514b5878011fbc0d6a94e
krishenriksen <github@krishenriksen.dk>
2021-04-17 20:14:20 +0700

    removed debug code

--- Wi-Fi.sh
+++ Wi-Fi.sh
@@ -52,15 +52,11 @@ Select() {
   # get password from input
   PASS=`$KEYBOARD -p "Enter Wi-Fi password" | tail -n 1`
 
-  echo $PASS > /home/ark/bla
-
   dialog --infobox "\nConnecting to Wi-Fi $1 ..." 5 55 > /dev/tty1
 
   # try to connect
   output=`nmcli con delete "$1"`
   output=`nmcli device wifi connect "$1" password "$PASS"`
-
-  echo $output > /home/ark/bla
 
   success=`echo "$output" | grep successfully`
 
